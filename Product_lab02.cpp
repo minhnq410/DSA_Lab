81c719c4124c8afae6bd167c4968ff99f0900251
minhnq410 <43316258+minhnq410@users.noreply.github.com>
2018-09-26 19:22:58 +0700

    Edit

--- Product_lab02.cpp
+++ Product_lab02.cpp
@@ -8,17 +8,14 @@ int main()
 {
     int m, n;
     cin >> m >> n;
-    int a = m;
-    cout << Product(m,n);
+    cout << Product(m, n);
     return 0;
 }
 
 int Product (int m, int n)
 {
-    int static a = m;
     if (m == 0 || n == 0) return 0;
     if (n == 1) return m;
     if (m == 1) return n;
-    m = m + a;
-    return Product (m, n - 1);
+    return m + Product (m, n - 1);
 }
